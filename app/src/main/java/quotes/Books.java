c2396a04092b72093589de8efc452a2490380d94
Ghaidagharaibeh-dot <ghedaghraibeh@yahoo.com>
2021-10-23 22:00:34 +0300

    lab 08

---
+++ app/src/main/java/quotes/Books.java
@@ -0,0 +1,12 @@
+package quotes;
+
+public class Books {
+    private String author ;
+    private String quotation ;
+
+    public Books(String author, String quotation) {
+        this.author = author;
+        this.quotation = quotation;
+    }
+
+}
